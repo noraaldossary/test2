e5fef703e085462e755026dac174e231b747b176
noraaldossary <nnxh.1@hotmail.com>
2023-10-05 12:06:20 +0300

    Initial Commit

---
+++ test2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test2
+//
+//  Created by Nora Aldossary on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Nora Aldossary on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
